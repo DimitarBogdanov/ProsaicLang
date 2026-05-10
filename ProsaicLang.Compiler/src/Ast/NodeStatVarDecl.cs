9e1b0391c333a26e26e5a9bdf500f629c80a4d96
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2026-05-10 18:06:05 +0300

    Add improved NiceName for var decl

--- ProsaicLang.Compiler/src/Ast/NodeStatVarDecl.cs
+++ ProsaicLang.Compiler/src/Ast/NodeStatVarDecl.cs
@@ -8,11 +8,15 @@ public sealed class NodeStatVarDecl : NodeStat
     public NodeStatVarDecl() : base("var declaration")
     {
     }
-    
-    
+
+
     public required Token NameToken { get; init; }
     public required TypeRef? SpecifiedType { get; init; }
     public required NodeExpr? Initializer { get; init; }
-    
+
     public string Name => NameToken.Value;
+
+    public override string NiceName => Initializer != null
+        ? $"var {Name}{(SpecifiedType != null ? $": {SpecifiedType.Name}" : "")} = {Initializer.NiceName};"
+        : Name;
 }
